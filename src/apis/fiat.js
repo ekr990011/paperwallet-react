1d2b7b92bf0e1784a4de6d48fe406717b4073c08
DooM <ekr990011@protonmail.com>
2018-11-28 11:40:17 -0600

    fiat cleanup

--- src/apis/fiat.js
+++ src/apis/fiat.js
@@ -1,15 +1,14 @@
 import axios from 'axios';
 
 export const fiatPriceCheck = (cryptoName, handlefiatPrice, resolve, reject) => {
-    axios.get("https://api.coingecko.com/api/v3/coins/" + cryptoName.toLowerCase() + "?localization=false&tickers=false&community_data=false&developer_data=false&sparkline=false", {
-      headers: {'content-type': 'application/x-www-form-urlencoded'}
-    }).then(res => {
-        // console.log(res.data.market_data.current_price);
-        const price = res.data.market_data.current_price.usd;
-        handlefiatPrice(price);
-        resolve('Fiat Done');
-      }).catch(error => {
-        console.log(error);
-        reject(error);
-      });
+  axios.get("https://api.coingecko.com/api/v3/coins/" + cryptoName.toLowerCase() + "?localization=false&tickers=false&community_data=false&developer_data=false&sparkline=false")
+  .then(res => {
+    // console.log(res.data.market_data.current_price);
+    const price = res.data.market_data.current_price.usd;
+    handlefiatPrice(price);
+    resolve('Fiat Done');
+  }).catch(error => {
+    console.log(error);
+    reject(error);
+  });
 };
